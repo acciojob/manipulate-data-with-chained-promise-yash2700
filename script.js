de0e11cbd7c8d4311b3b23eaca174046f89e45a0
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-04-03 09:27:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let array=[1,2,3,4]
 new Promise((resolve)=>{
-	setTimeout(()=>{resolve(array.filter((i)=>{return i%2!=0}))},1000);
+	setTimeout(()=>{resolve(array.filter((i)=>{return i%2==0}))},1000);
 }).then((value)=>{
 	document.getElementById("output").innerHTML=value;
 }).then((value)=>{
